8b094883233111da6262ca76caec532be5fce18c
douglas-cavalcante <henriquedouglas2803@gmail.com>
2021-08-07 00:21:49 -0300

    melhora import de presencas

--- app/Controllers/Http/AttendanceController.js
+++ app/Controllers/Http/AttendanceController.js
@@ -1,10 +1,10 @@
 'use strict'
 
 const Helpers = use('Helpers')
-
+const Contract = use('App/Models/Contract')
 let excelToJson = require('convert-excel-to-json');
 
-
+const {isValid} = require('date-fns')
 const Attendance = use('App/Models/Attendance')
 
 const groupBy = (key) => {
@@ -48,6 +48,16 @@ class AttendanceController {
         return file.error()
       }
 
+      const traineesContractValid = await Contract
+      .query()
+      .with('trainee')
+      .where("status", true)
+      .fetch();
+
+      const parseArrayTraineesContractValid = traineesContractValid.toJSON().reduce((acc, current) => {
+       return [...acc, current.trainee.cpf]
+      }, []);
+
       const json = excelToJson({
         sourceFile: Helpers.tmpPath('/uploads/presencas-file.xlsx')
       });
@@ -64,15 +74,17 @@ class AttendanceController {
           currentCpf = '0' + currentCpf;
         }
 
+        const parseDate = new Date(item['E'].toString().replace(/"/g, ''));
+
+        const dateIsValid = isValid(parseDate)
+
         return {
           cpf: currentCpf,
-          date: (item['E'] === 'NULL' || item['E'] === null)
-            ? ''
-            : new Date(item['E'].toString().replace(/"/g, '').substring(0, 10))
+          date: dateIsValid ? parseDate : ''
         }
       })
 
-      const filterJSON = parseJSON.filter(item => item.cpf.length === 11 && item.date)
+      const filterJSON = parseJSON.filter(item => (item.cpf.length === 11) && item.date)
 
       const groupDocument = groupBy('cpf');
 
@@ -80,24 +92,60 @@ class AttendanceController {
 
       const keys = Object.keys(data);
 
-      keys.forEach(async key => {
+
+      const rows = keys.map( async key => {
+
         const currentData = data[key];
-        const ordenedItems = currentData.slice().sort((a, b) => b.date - a.date)
-        await Attendance.findOrCreate({
+
+        const ordenedItems = currentData.slice().sort((a, b) => b.date - a.date);
+
+        const formattedCpf = ordenedItems[0].cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4")
+
+        if(!parseArrayTraineesContractValid.includes(formattedCpf)) {
+          return {
+            invalid: true
+          }
+        }
+
+        const attendanceExists = await Attendance.findBy({
           cpf: ordenedItems[0].cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4"),
           date: ordenedItems[0].date,
-        },
+
+        })
+
+        if(attendanceExists) {
+          return {
+            invalid: false,
+            attendance: attendanceExists,
+            exists: true
+          };
+        }
+
+        const attendance = await Attendance.create(
           {
             cpf: ordenedItems[0].cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, "$1.$2.$3-$4"),
             date: ordenedItems[0].date,
             type: params.type
-          });
+          }
+        );
+
+        return {
+          invalid: false,
+          attendance,
+          exists: false
+        };
       });
+
+      const result = await Promise.all(rows);
+
+      return {
+        count: result.filter(item => item.invalid === false)
+      }
 
     } catch (error) {
       return response
-        .status(error.message)
-        .send(error)
+        .status(error.status)
+        .send(error.message)
     }
   }
 
